4dbdaace757218535e113d318169705eb6d3c907
Patrick Kodal <patrick@kodal.dev>
2023-05-03 13:59:31 +0200

    prolog: space-age

---
+++ prolog/space-age/space_age.pl
@@ -0,0 +1,8 @@
+space_age("Mercury", Age, Years) :- Years = Age/31557600/0.2408467.
+space_age("Venus", Age, Years) :- Years = Age/31557600/0.61519726.
+space_age("Earth", Age, Years) :- Years = Age/31557600.
+space_age("Mars", Age, Years) :- Years = Age/31557600/1.8808158.
+space_age("Jupiter", Age, Years) :- Years = Age/31557600/11.862615.
+space_age("Saturn", Age, Years) :- Years = Age/31557600/29.447498.
+space_age("Uranus", Age, Years) :- Years = Age/31557600/84.016846.
+space_age("Neptune", Age, Years) :- Years = Age/31557600/164.79132.

5cff4641c4526a9a3274fdb2856a0ff52a61b0c9
Patrick Kodal <patrick@kodal.dev>
2023-05-24 10:40:09 +0200

    go: armstrong-numbers - iteration 4

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -5,10 +5,8 @@ import (
 )
 
 func IsNumber(number int) bool {
-	var (
-		digits           = float64(int(math.Log10(float64(number)) + 1))
-		calculatedNumber float64
-	)
+	digits := float64(int(math.Log10(float64(number)) + 1))
+	var calculatedNumber float64
 	for digitBase := number; digitBase > 0; {
 		calculatedNumber += math.Pow(float64(digitBase%10), digits)
 		digitBase /= 10

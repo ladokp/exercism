c7d4ac50725bf5ac8a6331f241137691e056f338
Patrick Kodal <patrick@kodal.dev>
2023-05-28 23:58:17 +0200

    go: bottle-song - iteration 2

--- go/bottle-song/bottle_song.go
+++ go/bottle-song/bottle_song.go
@@ -8,31 +8,30 @@ import (
 var numberToWord = []string{"No", "One", "Two", "Three", "Four", "Five",
 	"Six", "Seven", "Eight", "Nine", "Ten"}
 
-func Recite(startBottles, takeDown int) []string {
-	var res []string
+func Recite(startBottles, takeDown int) (result []string) {
 	stopBottles := startBottles - takeDown
 	for i := startBottles; i > stopBottles; i-- {
 		if i < startBottles {
-			res = append(res, "")
+			result = append(result, "")
 		}
-		res = append(res, verse(i)...)
+		result = append(result, verse(i)...)
 	}
-	return res
+	return
 }
 
-func verse(n int) []string {
+func verse(number int) []string {
 	line1 := fmt.Sprintf("%s green bottle%s hanging on the wall,",
-		numberToWord[n], plural(n))
+		numberToWord[number], plural(number))
 	line2 := "And if one green bottle should accidentally fall,"
 	line3 := fmt.Sprintf("There'll be %s green bottle%s hanging on the wall.",
-		strings.ToLower(numberToWord[n-1]), plural(n-1))
+		strings.ToLower(numberToWord[number-1]), plural(number-1))
 
 	return []string{line1, line1, line2, line3}
 }
 
-func plural(n int) (res string) {
-	if n != 1 {
-		res = "s"
+func plural(number int) (result string) {
+	if number != 1 {
+		result = "s"
 	}
-	return res
+	return
 }

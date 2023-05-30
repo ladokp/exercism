d2c1b2a47509585a9409490771749b8f46d26c06
Patrick Kodal <patrick@kodal.dev>
2023-05-30 22:56:41 +0200

    go: prime-factors - iteration 2

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,10 +1,18 @@
 package prime
 
 func Factors(number int64) (factors []int64) {
-	for factor := int64(2); number > 1; factor++ {
-		for ; number%factor == 0; number /= factor {
+	for number%2 == 0 {
+		factors = append(factors, 2)
+		number /= 2
+	}
+	for factor := int64(3); factor*factor <= number; factor += 2 {
+		for number%factor == 0 {
 			factors = append(factors, factor)
+			number /= factor
 		}
+	}
+	if number > 2 {
+		factors = append(factors, number)
 	}
 	return
 }

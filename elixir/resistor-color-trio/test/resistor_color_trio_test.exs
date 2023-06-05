eb17c72ce9a9f15e8695504eda5a626d414b1621
Patrick Kodal <patrick@kodal.dev>
2023-06-05 19:42:21 +0200

    elixir: resistor-color-trio - iteration 2

--- elixir/resistor-color-trio/test/resistor_color_trio_test.exs
+++ elixir/resistor-color-trio/test/resistor_color_trio_test.exs
@@ -10,7 +10,7 @@ defmodule ResistorColorTrioTest do
     assert output == expected
   end
 
-  # @tag :pending
+  @tag :pending
   test "Blue and grey and brown" do
     colors = [:blue, :grey, :brown]
     output = ResistorColorTrio.label(colors)
@@ -19,7 +19,7 @@ defmodule ResistorColorTrioTest do
     assert output == expected
   end
 
-  # @tag :pending
+  @tag :pending
   test "Red and black and red" do
     colors = [:red, :black, :red]
     output = ResistorColorTrio.label(colors)
@@ -28,7 +28,7 @@ defmodule ResistorColorTrioTest do
     assert output == expected
   end
 
-  # @tag :pending
+  @tag :pending
   test "Green and brown and orange" do
     colors = [:green, :brown, :orange]
     output = ResistorColorTrio.label(colors)
@@ -37,11 +37,56 @@ defmodule ResistorColorTrioTest do
     assert output == expected
   end
 
-  # @tag :pending
+  @tag :pending
   test "Yellow and violet and yellow" do
     colors = [:yellow, :violet, :yellow]
     output = ResistorColorTrio.label(colors)
     expected = {470, :kiloohms}
+
+    assert output == expected
+  end
+
+  @tag :pending
+  test "Blue and violet and blue" do
+    colors = [:blue, :violet, :blue]
+    output = ResistorColorTrio.label(colors)
+    expected = {67, :megaohms}
+
+    assert output == expected
+  end
+
+  @tag :pending
+  test "Minimum possible value" do
+    colors = [:black, :black, :black]
+    output = ResistorColorTrio.label(colors)
+    expected = {0, :ohms}
+
+    assert output == expected
+  end
+
+  @tag :pending
+  test "Maximum possible value" do
+    colors = [:white, :white, :white]
+    output = ResistorColorTrio.label(colors)
+    expected = {99, :gigaohms}
+
+    assert output == expected
+  end
+
+  @tag :pending
+  test "First two colors make an invalid octal number" do
+    colors = [:black, :grey, :black]
+    output = ResistorColorTrio.label(colors)
+    expected = {8, :ohms}
+
+    assert output == expected
+  end
+
+  @tag :pending
+  test "Ignore extra colors" do
+    colors = [:blue, :green, :yellow, :orange]
+    output = ResistorColorTrio.label(colors)
+    expected = {650, :kiloohms}
 
     assert output == expected
   end

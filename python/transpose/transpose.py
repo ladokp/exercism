0e355d94fc03fa6f2db035a7366186f0bd6768f6
Patrick Kodal <patrick@kodal.dev>
2023-06-09 22:23:39 +0200

    python: transpose - iteration 2

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,5 +1,12 @@
 import itertools
 
+
 def transpose(lines):
-    transposed_list = ["".join(line).rstrip("~").replace("~", " ") for line in itertools.zip_longest(*lines.splitlines(), fillvalue="~")]
+    def custom_trim(line):
+        return "".join(line).rstrip("~").replace("~", " ")
+
+    transposed_list = [
+        custom_trim(line)
+        for line in itertools.zip_longest(*lines.splitlines(), fillvalue="~")
+    ]
     return "\n".join(transposed_list)

1dd4a98d9d4c892f21dab7c0ea6001deb55730c0
Patrick Kodal <patrick@kodal.dev>
2023-08-07 16:17:39 +0200

    typescript: sum-of-multiples - iteration 2

--- typescript/sum-of-multiples/sum-of-multiples.ts
+++ typescript/sum-of-multiples/sum-of-multiples.ts
@@ -1,7 +1,7 @@
-export function sum(mutiples: number[], N: number): number {
+export function sum(multiples: number[], N: number): number {
   let result = 0;
   for (let currentNumber = 1; currentNumber < N; currentNumber++) {
-    if (mutiples.some((multiple) => currentNumber % multiple === 0)) result += currentNumber;
+    if (multiples.some((multiple) => currentNumber % multiple === 0)) result += currentNumber;
   }
   return result;
 }

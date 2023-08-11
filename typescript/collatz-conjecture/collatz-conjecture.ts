31f8d20bdcf39d4bb037a688c5404b4a272701ac
Patrick Kodal <patrick@kodal.dev>
2023-08-11 17:39:33 +0200

    typescript: collatz-conjecture

---
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -0,0 +1,6 @@
+export function steps(count: number): number {
+  if (count <= 0 || !Number.isInteger(count)) throw new Error('Only positive integers are allowed');
+  if (count === 1) return 0;
+  if (count % 2) return 1 + steps(3 * count + 1);
+  return 1 + steps(Math.floor(count/2));
+}

2f354f7d10a90ff79ffc65b52ba38e258984f423
Patrick Kodal <patrick@kodal.dev>
2023-08-23 21:27:52 +0200

    typescript: robot-name - iteration 3

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,33 +1,27 @@
-const UPPERCASE_CHARACTERS ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
 export class Robot {
   private static usedNames = new Set<string>();
   
-  constructor(
-    private _name: string = '#####'
-  ) {}
+  constructor(private _name: string) {
+    this.resetName();
+  }
 
-  private generateString(length: number): string {
+  private generateString(): string {
     let result = '';
-    const charactersLength = UPPERCASE_CHARACTERS.length;
-    for ( let i = 0; i < length; i++ ) {
-      result += UPPERCASE_CHARACTERS.charAt(Math.floor(Math.random() * charactersLength));
+    for ( let i = 0; i < 2; i++ ) {
+      result += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(Math.floor(Math.random() * 26));
     }
     return result;
   }
   
   public get name(): string {
-    if (this._name === '#####') {
-      do {
-        this._name = this.generateString(2) + Math.floor(Math.random() * (999 - 100 + 1) + 100)
-      } while (Robot.usedNames.has(this._name))
-      Robot.usedNames.add(this._name);
-    }
     return this._name;
   }
 
   public resetName(): void {
-    this._name = '#####';
+    do {
+      this._name = this.generateString() + Math.floor(Math.random() * (999 - 100 + 1) + 100)
+    } while (Robot.usedNames.has(this._name))
+    Robot.usedNames.add(this._name);
   }
 
   public static releaseNames(): void {

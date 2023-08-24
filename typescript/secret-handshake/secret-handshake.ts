99d0a38dd405cb8dd43a83a1de53382c99ec1c32
Patrick Kodal <patrick@kodal.dev>
2023-08-24 21:01:14 +0200

    typescript: secret-handshake - iteration 2

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -1,9 +1,9 @@
 export function commands(input: number): string[] {
   const movements: string[] = [];
-  if (input & 1) movements.push("wink");
-  if (input & 2) movements.push("double blink");
-  if (input & 4) movements.push("close your eyes");
-  if (input & 8) movements.push("jump");
-  if (input & 16) movements.reverse();
+  if (input & 0B00001) movements.push("wink");
+  if (input & 0B00010) movements.push("double blink");
+  if (input & 0B00100) movements.push("close your eyes");
+  if (input & 0B01000) movements.push("jump");
+  if (input & 0B10000) movements.reverse();
   return movements;
 }

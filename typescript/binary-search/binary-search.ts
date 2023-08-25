3ab4763644183cd1cae6379b3f65824ea7bcf1ec
Patrick Kodal <patrick@kodal.dev>
2023-08-25 18:51:37 +0200

    typescript: binary-search - iteration 3

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,15 +1,16 @@
 export function find(haystack: number[], needle: number, left: number=0, right: number=(haystack.length-1)): number | never {
-  let middle = Math.floor(((right-left)/2) + left)
+  let mid = Math.floor((left + right) / 2)
 
   switch (true) {
-    case haystack[middle] === needle:
-      return middle
+    case haystack[mid] === needle:
+      return mid
     case right - left === 0:
       break
-    case haystack[middle] < needle:
-      return find(haystack, needle, middle+1, right)
-    case haystack[middle] > needle:
-      return find(haystack, needle, left, middle-1)
+    case haystack[mid] < needle:
+      return find(haystack, needle, mid+1, right)
+    case haystack[mid] > needle:
+      return find(haystack, needle, left, mid-1)
   }
+  
   throw new Error('Value not in array')
 }

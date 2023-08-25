8d5c3ae27e49d3b46c0a3265178ea82cc92f9024
Patrick Kodal <patrick@kodal.dev>
2023-08-25 21:58:42 +0200

    typescript: secret-handshake - iteration 4

--- typescript/secret-handshake/secret-handshake.ts
+++ typescript/secret-handshake/secret-handshake.ts
@@ -3,6 +3,5 @@ export function commands(input: number): string[] {
   const MOVEMENTS: string[] = [];
   for (let counter = 0; counter < 4; counter++)
     if (input & (1 << counter)) MOVEMENTS.push(ACTIONS[counter]);
-  if (input & 0B10000) MOVEMENTS.reverse();
-  return MOVEMENTS;
+  return input & 0B10000 ? MOVEMENTS.reverse() : MOVEMENTS;
 }

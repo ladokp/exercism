59e2f86ef38af1568bbbb6950317b47807104e8d
Patrick Kodal <patrick@kodal.dev>
2023-08-29 20:20:43 +0200

    powershell: darts - iteration 2

--- powershell/darts/Darts.ps1
+++ powershell/darts/Darts.ps1
@@ -24,14 +24,8 @@ Function Invoke-Darts() {
     )
     
     $distance = [Math]::Sqrt([Math]::Pow($X, 2) + [Math]::Pow($Y, 2))
-    if ($distance -le 1) {
-        return 10
-    }
-    if ($distance -le 5) {
-        return 5
-    }
-    if ($distance -le 10) {
-        return 1
-    }
+    if ($distance -le 1) { return 10 }
+    if ($distance -le 5) { return 5 }
+    if ($distance -le 10) { return 1 }
     return 0
 }

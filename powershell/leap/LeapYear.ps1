2b6ca569beb848a698a4e017dd05c56364c6f567
Patrick Kodal <patrick@kodal.dev>
2023-08-29 20:13:32 +0200

    powershell: leap - iteration 2

--- powershell/leap/LeapYear.ps1
+++ powershell/leap/LeapYear.ps1
@@ -26,5 +26,5 @@ function Test-LeapYear {
     #>
     param( [int]$year )
 
-    return ($year % 4 -eq 0 -and $year % 100 -ne 0) -or $year % 400 -eq 0
+    ($year % 4 -eq 0 -and $year % 100 -ne 0) -or $year % 400 -eq 0
 }

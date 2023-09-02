27c1059b64d843d6afaed0f5e1d466b28a35059b
Patrick Kodal <patrick@kodal.dev>
2023-09-02 21:55:30 +0200

    go: blackjack - iteration 3

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -1,73 +1,64 @@
 package blackjack
 
+var CARDS = map[string]int{
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"ten":   10,
+	"jack":  10,
+	"queen": 10,
+	"king":  10,
+	"ace":   11,
+}
+
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	switch card {
-        case "ace":
-    		return 11
-        case "two":
-    		return 2
-        case "three":
-    		return 3
-        case "four":
-    		return 4
-        case "five":
-    		return 5
-        case "six":
-    		return 6
-        case "seven":
-    		return 7
-        case "eight":
-    		return 8
-        case "nine":
-    		return 9
-        case "ten":
-    		return 10
-        case "jack":
-    		return 10
-        case "queen":
-    		return 10
-        case "king":
-    		return 10
-        default:
-    		return 0
-    }
+	if points, found := CARDS[card]; found {
+		return points
+	} else {
+		return 0
+	}
 }
 
 // IsBlackjack returns true if the player has a blackjack, false otherwise.
 func IsBlackjack(card1, card2 string) bool {
-	return ParseCard(card1) + ParseCard(card2) == 21
+	return ParseCard(card1)+ParseCard(card2) == 21
 }
 
 // LargeHand implements the decision tree for hand scores larger than 20 points.
 func LargeHand(isBlackjack bool, dealerScore int) string {
 	if isBlackjack && dealerScore < 10 {
-        return "W"
-    } 
+		return "W"
+	}
 	if !isBlackjack {
-    	return "P"
-    }
+		return "P"
+	}
 	return "S"
 }
 
 // SmallHand implements the decision tree for hand scores with less than 21 points.
 func SmallHand(handScore, dealerScore int) string {
-    if handScore >= 17 {
-        return "S"
-    } else if handScore <= 11 {
-    	return "H"
-    } else if dealerScore < 7 {
-    	return "S"
-    } else {
-    	return "H"
-    }
+	if handScore >= 17 {
+		return "S"
+	} else if handScore <= 11 {
+		return "H"
+	} else if dealerScore < 7 {
+		return "S"
+	} else {
+		return "H"
+	}
 }
 
 func FirstTurn(card1, card2, dealerCard string) string {
-    ownScore := ParseCard(card1)+ParseCard(card2)
-    dealerScore := ParseCard(dealerCard)
-    if (ownScore < 21) {
-        return SmallHand(ownScore, dealerScore)
-    }
+	ownScore := ParseCard(card1) + ParseCard(card2)
+	dealerScore := ParseCard(dealerCard)
+	if ownScore < 21 {
+		return SmallHand(ownScore, dealerScore)
+	}
 	return LargeHand(IsBlackjack(card1, card2), dealerScore)
 }

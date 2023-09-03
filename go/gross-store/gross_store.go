400e091d258d91221cc18446f52ef4f4501e0716
Patrick Kodal <patrick@kodal.dev>
2023-09-03 19:36:50 +0200

    go: gross-store - iteration 2

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -3,13 +3,13 @@ package gross
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
 	return map[string]int{
-        "quarter_of_a_dozen": 3,
-        "half_of_a_dozen": 6,
-        "dozen": 12,
-        "small_gross": 120,
-        "gross": 144,
-        "great_gross": 1728,
-    }
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 }
 
 // NewBill creates a new bill.
@@ -20,28 +20,28 @@ func NewBill() map[string]int {
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
 	amount, exists := units[unit]
-    if !exists {
-        return false
-    }
+	if !exists {
+		return false
+	}
 
 	bill[item] = bill[item] + amount
-    return true
+	return true
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-    quantity, item_exists := bill[item]
-	amount, unit_exists := units[unit]
-	if !item_exists || !unit_exists || quantity - amount < 0 {
-        return false
-    }
+	quantity, itemExists := bill[item]
+	amount, unitExists := units[unit]
+	if !itemExists || !unitExists || quantity-amount < 0 {
+		return false
+	}
 
-	if quantity - amount == 0 {
-        delete(bill, item)
-    } else {
+	if quantity-amount == 0 {
+		delete(bill, item)
+	} else {
 		bill[item] = bill[item] - amount
-    }
-    return true
+	}
+	return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.

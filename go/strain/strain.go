d644170bd1abed3a72b821b1c7b5806c459fac9b
Patrick Kodal <patrick@kodal.dev>
2023-09-12 20:45:30 +0200

    go: strain - iteration 2

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,36 +1,15 @@
 package strain
 
-type Ints []int
-type Lists [][]int
-type Strings []string
-
-func (i Ints) Keep(filter func(int) bool) (output Ints) {
-	for _, number := range i {
-		if filter(number) {
-			output = append(output, number)
+func Keep[T any](list []T, filter func(T) bool) []T {
+	result := []T{}
+	for _, element := range list {
+		if filter(element) {
+			result = append(result, element)
 		}
 	}
-	return
+	return result
 }
 
-func (i Ints) Discard(filter func(int) bool) Ints {
-	return i.Keep(func(number int) bool { return !filter(number) })
-}
-
-func (l Lists) Keep(filter func([]int) bool) (output Lists) {
-	for _, number := range l {
-		if filter(number) {
-			output = append(output, number)
-		}
-	}
-	return
-}
-
-func (s Strings) Keep(filter func(string) bool) (output Strings) {
-	for _, word := range s {
-		if filter(word) {
-			output = append(output, word)
-		}
-	}
-	return
+func Discard[T any](list []T, filter func(T) bool) []T {
+	return Keep(list, func(element T) bool { return !filter(element) })
 }

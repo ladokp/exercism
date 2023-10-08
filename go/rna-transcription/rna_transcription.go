329f678968222d5b1fe1a0781aad1bf9ec3993a1
Patrick Kodal <patrick@kodal.dev>
2023-10-08 09:16:36 +0200

    go: rna-transcription - iteration 2

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -2,13 +2,14 @@ package strand
 
 func ToRNA(dna string) string {
 	rna := make([]byte, len(dna))
+	translationMap := map[rune]byte{
+		'G': 'C',
+		'C': 'G',
+		'T': 'A',
+		'A': 'U',
+	}
 	for index, character := range dna {
-		rna[index] = map[rune]byte{
-			'G': 'C',
-			'C': 'G',
-			'T': 'A',
-			'A': 'U',
-		}[character]
+		rna[index] = translationMap[character]
 	}
 	return string(rna)
 }

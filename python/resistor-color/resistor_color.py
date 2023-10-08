75ebb22e1e82a6d068334c038bdcd8e34f8d668b
Patrick Kodal <patrick@kodal.dev>
2023-10-08 11:34:45 +0200

    python: resistor-color - iteration 2

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,20 +1,20 @@
-RESISTOR_COLOR_DICT = {
-    "black": 0,
-    "brown": 1,
-    "red": 2,
-    "orange": 3,
-    "yellow": 4,
-    "green": 5,
-    "blue": 6,
-    "violet": 7,
-    "grey": 8,
-    "white": 9,
-}
+RESISTOR_COLORS = (
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+)
 
 
 def color_code(color):
-    return RESISTOR_COLOR_DICT.get(color)
+    return RESISTOR_COLORS.index(color)
 
 
 def colors():
-    return list(RESISTOR_COLOR_DICT.keys())
+    return list(RESISTOR_COLORS)

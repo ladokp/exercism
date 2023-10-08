a7bc8edf944e147c56eed609b7f65bc94e86c69c
Patrick Kodal <patrick@kodal.dev>
2023-10-08 20:38:16 +0200

    python: triangle - iteration 2

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,20 +1,20 @@
 def is_valid_triangle(f):
     def inner(sides):
-        return sum(sides) > 2 * max(sides) and f(sides)
+        return sum(sides) > 2 * max(sides) and f(len(set(sides)))
 
     return inner
 
 
 @is_valid_triangle
-def equilateral(sides):
-    return len(set(sides)) == 1
+def equilateral(number_of_sides):
+    return number_of_sides == 1
 
 
 @is_valid_triangle
-def isosceles(sides):
-    return len(set(sides)) < 3
+def isosceles(number_of_sides):
+    return number_of_sides < 3
 
 
 @is_valid_triangle
-def scalene(sides):
-    return len(set(sides)) == 3
+def scalene(number_of_sides):
+    return number_of_sides == 3

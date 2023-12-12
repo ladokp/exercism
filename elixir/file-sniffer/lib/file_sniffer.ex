fa7015dd36aea476293f1a2bb0c5329ef12f4a4f
Patrick Kodal <patrick@kodal.dev>
2023-12-12 06:24:26 +0100

    elixir: file-sniffer - iteration 3

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,24 +1,18 @@
 defmodule FileSniffer do
- @types [
-    # {extension, type, binary}
-    {"exe", "application/octet-stream", <<0x7F, 0x45, 0x4C, 0x46>>},
-    {"bmp", "image/bmp", <<0x42, 0x4D>>},
-    {"png", "image/png", <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>},
-    {"jpg", "image/jpg", <<0xFF, 0xD8, 0xFF>>},
-    {"gif", "image/gif", <<0x47, 0x49, 0x46>>}
-  ]
+  def type_from_extension("bmp"), do: "image/bmp"
+  def type_from_extension("png"), do: "image/png"
+  def type_from_extension("jpg"), do: "image/jpg"
+  def type_from_extension("gif"), do: "image/gif"
+  def type_from_extension("exe"), do: "application/octet-stream"
+  def type_from_extension(_file_extension), do: nil
 
-  def type_from_extension(extension) do
-    Enum.find_value(@types, fn {ext, type, _} ->
-      if ext == extension, do: type
-    end)
-  end
-
-  def type_from_binary(file_binary) do
-    Enum.find_value(@types, fn {_, type, binary} ->
-      if String.starts_with?(file_binary, binary), do: type
-    end)
-  end
+  def type_from_binary(<<0x42, 0x4D, _::binary>>), do: "image/bmp"
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>),
+    do: "image/png"
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>), do: "image/jpg"
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>), do: "image/gif"
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>), do: "application/octet-stream"
+  def type_from_binary(_binary), do: nil
 
   def verify(file_binary, extension) do
     type1 = type_from_binary(file_binary)

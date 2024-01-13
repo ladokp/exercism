8196fbd0feeefbc6bb182e1dff542029ee123dab
Patrick Kodal <patrick@kodal.dev>
2024-01-13 21:42:57 +0100

    python: leap - iteration 4

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -3,13 +3,5 @@ A function to report if a given year is a leap year.
 """
 
 
-def generate_leap_years(limit_year):
-    return (
-        year
-        for year in range(1, limit_year + 1)
-        if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
-    )
-
-
 def leap_year(year):
-    return year in generate_leap_years(year)
+    return not year % 400 if not year % 100 else not year % 4

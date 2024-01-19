7911953c2e7d1f537f15982181eff0af61163801
Patrick Kodal <patrick@kodal.dev>
2024-01-19 20:39:27 +0100

    python: leap - iteration 5

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -4,4 +4,4 @@ A function to report if a given year is a leap year.
 
 
 def leap_year(year):
-    return not year % 400 if not year % 100 else not year % 4
+    return not year & 3 and (year % 25 != 0 or not year & 15)

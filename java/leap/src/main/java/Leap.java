63d813d6092824786ca789d60f52e690e999c355
Patrick Kodal <patrick@kodal.dev>
2024-01-25 22:04:08 +0100

    java: leap - iteration 4

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,28 @@
+@FunctionalInterface
+interface LeapYearChecker { 
+    boolean isLeapYear(int year); 
+}
+
 class Leap {
 
-    boolean isLeapYear(int year) {
-        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    protected LeapYearChecker checker;
+
+    public Leap() {
+        checker = (year) -> year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 
+    public boolean isLeapYear(int year) {
+        return checker.isLeapYear(year);
+    }
+    
+}
+
+class GregorianLeap extends Leap {}
+
+class JulianLeap extends Leap {
+
+    public JulianLeap() {
+        checker = (year) -> year % 4 == 0;
+    }
+    
 }

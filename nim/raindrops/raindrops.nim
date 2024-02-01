743ed9bf483fa9660c20b6e2b8657210a64ecd28
Patrick Kodal <patrick@kodal.dev>
2024-02-01 12:44:47 +0100

    nim: raindrops

---
+++ nim/raindrops/raindrops.nim
@@ -0,0 +1,5 @@
+proc convert*(number: int): string =
+  if number mod 3 == 0: result.add "Pling"
+  if number mod 5 == 0: result.add "Plang"
+  if number mod 7 == 0: result.add "Plong"
+  if result == "": result.add $number

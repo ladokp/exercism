891fc970d3e3319e12fe2681dd2cd1560f743dfa
Patrick Kodal <patrick@kodal.dev>
2024-02-05 20:53:02 +0100

    java: squeaky-clean - iteration 2

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -6,9 +6,15 @@ class SqueakyClean {
   static String clean(String identifier) {
 
     var result = identifier.replaceAll(" ", "_")
+        .replaceAll("0", "o")
+        .replaceAll("1", "l")
+        .replaceAll("3", "e")
+        .replaceAll("4", "a")
+        .replaceAll("7", "t")
         .replaceAll("\\p{C}", "CTRL")
         .replaceAll("[^\\p{L}\\p{P}]", "")
-        .replaceAll("[α-ω]", "");
+        .replaceAll("[α-ω]", "")
+        .replaceAll("[^a-zA-Z_-]", "");
 
     var matcher = Pattern.compile("(.*)(-\\p{L})(.*)").matcher(result);
     if (matcher.matches()) {

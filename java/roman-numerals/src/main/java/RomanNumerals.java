f6c74f0cd5a9ba294cd9357d422a206763212375
Patrick Kodal <patrick@kodal.dev>
2024-02-06 19:13:52 +0100

    java: roman-numerals - iteration 2

--- java/roman-numerals/src/main/java/RomanNumerals.java
+++ java/roman-numerals/src/main/java/RomanNumerals.java
@@ -1,66 +1,43 @@
-class RomanNumerals {
+import java.util.*;
+
+public class RomanNumerals {
     private int number;
     
-    RomanNumerals(int number) {
-        this.number = number;
+    private static NavigableMap<Integer, String> values = new TreeMap<>();
+    
+    static {
+        values.put(1_000,   "M");
+        values.put(  900,  "CM");
+        values.put(  500,   "D");
+        values.put(  400,  "CD");
+        values.put(  100,   "C");
+        values.put(   90,  "XC");
+        values.put(   50,   "L");
+        values.put(   40,  "XL");
+        values.put(   10,   "X");
+        values.put(    9,  "IX");
+        values.put(    5,   "V");
+        values.put(    4,  "IV");
+        values.put(    1,   "I");
+        values = values.descendingMap();
     }
 
+    public RomanNumerals(int number) {
+        this.number = number;
+    }
+    
     String getRomanNumeral() {
-        StringBuilder resultBuilder = new StringBuilder();
-        while (this.number > 0) {
-            if (this.number >= 1000) {
-                resultBuilder.append("M"); 
-                this.number -= 1000;                  
-            }
-            else if (this.number >= 900) {
-                resultBuilder.append("CM"); 
-                this.number -= 900;                  
-            }
-            else if (this.number >= 500) {
-                resultBuilder.append("D"); 
-                this.number -= 500;                  
-            }
-            else if (this.number >= 400) {
-                resultBuilder.append("CD"); 
-                this.number -= 400;                  
-            }
-            else if (this.number >= 100) {
-                resultBuilder.append("C"); 
-                this.number -= 100;                  
-            }
-            else if (this.number >= 90) {
-                resultBuilder.append("XC"); 
-                this.number -= 90;                  
-            }
-            else if (this.number >= 50) {
-                resultBuilder.append("L"); 
-                this.number -= 50;                  
-            }
-            else if (this.number >= 40) {
-                resultBuilder.append("XL"); 
-                this.number -= 40;                  
-            }
-            else if (this.number >= 10) {
-                resultBuilder.append("X"); 
-                this.number -= 10;                  
-            }
-            else if (this.number >= 9) {
-                resultBuilder.append("IX"); 
-                this.number -= 9;                  
-            }
-            else if (this.number >= 5) {
-                resultBuilder.append("V"); 
-                this.number -= 5;                  
-            }
-            else if (this.number >= 4) {
-                resultBuilder.append("IV"); 
-                this.number -= 4;                  
-            }
-            else if (this.number >= 1) {
-                resultBuilder.append("I"); 
-                this.number -= 1;                  
+        StringBuilder numeralBuilder = new StringBuilder();
+
+        for (var entry : values.entrySet()) {
+            String numeral = entry.getValue();
+            int currentNumber = entry.getKey();
+
+            while (this.number >= currentNumber) {
+                this.number -= currentNumber;
+                numeralBuilder.append(numeral);
             }
         }
-        return resultBuilder.toString();
+        return numeralBuilder.toString();
     }
 }

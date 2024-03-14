4fc8aef611e8dfbd79248b68077b47b4cd6dfb30
Patrick Kodal <patrick@kodal.dev>
2024-03-14 21:31:02 +0100

    python: pangram - iteration 4

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,2 @@
-from string import ascii_lowercase
-
-
 def is_pangram(sentence):
-    return set(ascii_lowercase) - set(sentence.lower()) == set([])
+    return set("abcdefghijklmnopqrstuvwxyz") - set(sentence.lower()) == set([])

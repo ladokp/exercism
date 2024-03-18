4d33dc0ad3c0e41eec879982ff17cd63779f3d74
Patrick Kodal <patrick@kodal.dev>
2024-03-18 20:02:15 +0100

    nim: allergies

---
+++ nim/allergies/allergies.nim
@@ -0,0 +1,13 @@
+type Allergen* = enum
+    Eggs
+    Peanuts
+    Shellfish
+    Strawberries
+    Tomatoes
+    Chocolate
+    Pollen
+    Cats
+
+func isAllergicTo*(score: int, allergen: Allergen): bool = (score and 1 shl allergen.ord) != 0
+
+func allergies*(score: int): set[Allergen] = cast[typeof(result)](score)

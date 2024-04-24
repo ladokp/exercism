62e2f42a1b2ca91021d615833fa2f7cb8a950ea7
Patrick Kodal <patrick@kodal.dev>
2024-04-24 22:04:44 +0200

    sqlite: raindrops

---
+++ sqlite/raindrops/raindrops.sql
@@ -0,0 +1,9 @@
+-- Schema: CREATE TABLE "raindrops" ("number" INT, "sound" TEXT);
+-- Task: update the raindrops table and set the sound based on the number.
+UPDATE raindrops
+SET sound = IIF(number % 3 = 0, "Pling", "")
+            || IIF(number % 5 = 0, "Plang", "")
+            || IIF(number % 7 = 0, "Plong", "");
+
+UPDATE raindrops
+SET sound = IIF(sound = "", number, sound)

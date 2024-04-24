0a4b0e11e2cedf1f1bb3f442c2f0ab2a9b6b6ecf
Patrick Kodal <patrick@kodal.dev>
2024-04-24 20:30:09 +0200

    sqlite: difference-of-squares

---
+++ sqlite/difference-of-squares/difference-of-squares.sql
@@ -0,0 +1,12 @@
+-- Schema: CREATE TABLE "difference-of-squares" ("number" INT", property" TEXT, "result" INT);
+-- Task: update the difference-of-squares table and set the result based on the number and property fields.
+UPDATE "difference-of-squares"
+SET result = CASE property
+WHEN "squareOfSum" THEN 
+    pow((number * (number + 1) / 2), 2)
+WHEN "sumOfSquares" THEN 
+    number * (number + 1) * (2 * number + 1) / 6
+WHEN "differenceOfSquares" THEN
+    pow((number * (number + 1) / 2), 2) -
+    number * (number + 1) * (2 * number + 1) / 6
+END

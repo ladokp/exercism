73f4c9c7e0acfcf5e7b6bafe8bb54618fa1ee87a
Patrick Kodal <patrick@kodal.dev>
2024-05-06 21:44:26 +0200

    sqlite: collatz-conjecture

---
+++ sqlite/collatz-conjecture/collatz-conjecture.sql
@@ -0,0 +1,23 @@
+-- Schema: CREATE TABLE "collatz" ("number" INTEGER, "steps" INTEGER);
+-- Task: update the collatz table and set the steps based on the number.
+ALTER TABLE collatz
+ADD current INTEGER;
+
+UPDATE collatz
+SET steps = 0;
+
+PRAGMA recursive_triggers = ON;
+
+CREATE TRIGGER IF NOT EXISTS update_collatz
+	AFTER UPDATE
+    ON collatz
+    WHEN NEW.current != 1
+BEGIN
+	UPDATE collatz
+    SET current = IIF(NEW.current % 2, NEW.current * 3 + 1, NEW.current / 2),
+        steps = NEW.steps + 1
+    WHERE current = NEW.current;
+END;
+
+UPDATE collatz
+SET current = number;

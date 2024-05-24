3e387536a02fa5f615d55f4aed446e5df1e0ac28
Patrick Kodal <patrick@kodal.dev>
2024-05-24 18:48:04 +0200

    python: grains - iteration 3

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,8 +1,8 @@
 def square(number):
     if not 1 <= number <= 64:
         raise ValueError("square must be between 1 and 64")
-    return 2 ** (number - 1)
+    return 1 << (number - 1)
 
 
 def total():
-    return 2**64 - 1
+    return (1 << 64) - 1

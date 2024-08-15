16f3e41986cdbdc4120a4045df20253335505978
Patrick Kodal <patrick@kodal.dev>
2024-08-15 08:46:28 +0200

    python: diamond - iteration 2

--- python/diamond/diamond.py
+++ python/diamond/diamond.py
@@ -1,9 +1,10 @@
 def rows(letter):
-    letters = [
+    letters = tuple(
         chr(code_point) for code_point in range(ord("A"), ord(letter) + 1)
-    ]
-    alphabet = letters[:-1] + letters[::-1]
-    diamond_line = letters[::-1] + letters[1:]
+    )
     return [
-        "".join(x if x == y else " " for y in diamond_line) for x in alphabet
+        "".join(
+            x if x == y else " " for y in tuple(letters[::-1] + letters[1:])
+        )
+        for x in tuple(letters[:-1] + letters[::-1])
     ]

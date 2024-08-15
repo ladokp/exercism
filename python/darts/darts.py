6535d12361dc71f579e15725361345557c5cd313
Patrick Kodal <patrick@kodal.dev>
2024-08-15 19:45:41 +0200

    python: darts - iteration 4

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,6 +1,15 @@
 def score(x, y):
-    distance, rings = (x**2 + y**2) ** 0.5, (1, 5, 10)
-    for index, ring in enumerate(rings, start=1):
-        if distance <= ring:
-            return rings[-index]
+    distance = (x**2 + y**2) ** 0.5
+    hit_area = (
+        distance <= 1,
+        distance <= 5,
+        distance <= 10,
+    )
+    match hit_area:
+        case (True, *_):
+            return 10
+        case (_, True, _):
+            return 5
+        case (*_, True):
+            return 1
     return 0

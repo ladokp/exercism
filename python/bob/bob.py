dac9b5528ad673ef40d2fc1365e1210033f28642
Patrick Kodal <patrick@kodal.dev>
2024-08-15 19:11:22 +0200

    python: bob - iteration 3

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,15 @@
 def response(hey_bob):
-    if not (hey_bob := hey_bob.strip()):
-        return "Fine. Be that way!"
-    is_question = hey_bob.endswith("?")
-    is_yelling = hey_bob.isupper()
-    if is_question and is_yelling:
-        return "Calm down, I know what I'm doing!"
-    if is_question:
-        return "Sure."
-    if is_yelling:
-        return "Whoa, chill out!"
+    is_empty = not (hey_bob := hey_bob.strip())
+    is_question = not is_empty and hey_bob.endswith("?")
+    is_yelling = not is_empty and hey_bob.isupper()
+
+    match (is_empty, is_question, is_yelling):
+        case (True, _, _):
+            return "Fine. Be that way!"
+        case (_, True, True):
+            return "Calm down, I know what I'm doing!"
+        case (_, True, False):
+            return "Sure."
+        case (_, False, True):
+            return "Whoa, chill out!"
     return "Whatever."

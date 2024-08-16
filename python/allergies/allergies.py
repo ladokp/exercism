abe904a6764dda319f9ffee18a1cf94d56767c34
Patrick Kodal <patrick@kodal.dev>
2024-08-16 17:25:07 +0200

    python: allergies - iteration 2

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,20 +1,20 @@
 class Allergies:
-    ingredients = {
-        "eggs": 1,
-        "peanuts": 2,
-        "shellfish": 4,
-        "strawberries": 8,
-        "tomatoes": 16,
-        "chocolate": 32,
-        "pollen": 64,
-        "cats": 128,
-    }
+    ingredients = (
+        "eggs",
+        "peanuts",
+        "shellfish",
+        "strawberries",
+        "tomatoes",
+        "chocolate",
+        "pollen",
+        "cats",
+    )
 
     def __init__(self, score):
         self.score = score
 
     def allergic_to(self, item):
-        return bool(self.score & Allergies.ingredients.get(item))
+        return bool(self.score & 2 ** Allergies.ingredients.index(item))
 
     @property
     def lst(self):

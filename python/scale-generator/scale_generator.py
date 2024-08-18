20c906ace65bad55287f846d2d428afcd8eb83ad
Patrick Kodal <patrick@kodal.dev>
2024-08-18 21:57:43 +0200

    python: scale-generator - iteration 2

--- python/scale-generator/scale_generator.py
+++ python/scale-generator/scale_generator.py
@@ -20,17 +20,22 @@ STEPS = {"m": 1, "M": 2, "A": 3}
 
 
 class Scale:
+    """Class to represent a musical scale with a given tonic."""
+
     def __init__(self, tonic):
+        """Initialize the Scale with a tonic note and determine its scale type."""
         self.scale = SHARP_SCALE if tonic in USES_SHARPS else FLAT_SCALE
         self.tonic = tonic.capitalize()
 
     def chromatic(self):
+        """Return the chromatic scale starting from the tonic note."""
         return [
-            self.scale[(self.scale.index(self.tonic) + i) % 12]
-            for i in range(12)
+            self.scale[(self.scale.index(self.tonic) + index) % 12]
+            for index in range(12)
         ]
 
     def interval(self, intervals):
+        """Return the scale notes based on the specified intervals."""
         chromatic, index = self.chromatic(), 0
         scale = [chromatic[index]]
         for step in intervals:

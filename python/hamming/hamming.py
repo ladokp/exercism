4199c36592c6394cf663bf370d267c0ea41e7999
Patrick Kodal <patrick@kodal.dev>
2024-08-30 22:25:53 +0200

    python: hamming - iteration 2

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,8 @@
-def distance(strand_a, strand_b):
+def distance(strand_a: str, strand_b: str) -> int:
     """
     Calculating the Hamming Distance between two DNA strands.
     """
-    if len(strand_a) != len(strand_b):
-        raise ValueError("Strands must be of equal length.")
+    if not len(strand_a) - len(strand_b):
+        return sum(a != b for a, b in zip(strand_a, strand_b))
 
-    return sum(a != b for a, b in zip(strand_a, strand_b))
+    raise ValueError("Strands must be of equal length.")

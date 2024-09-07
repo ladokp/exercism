31f299422d6214bc5768f90e79cf0a887677c5b1
Patrick Kodal <patrick@kodal.dev>
2024-09-07 12:23:42 +0200

    python: spiral-matrix - iteration 2

--- python/spiral-matrix/spiral_matrix.py
+++ python/spiral-matrix/spiral_matrix.py
@@ -1,19 +1,29 @@
 from itertools import cycle
 
 
-def spiral_matrix(size):
-    matrix = [[None] * size for _ in range(size)]
-    r_index, c_index = 0, 0
-    deltas = cycle(((0, 1), (1, 0), (0, -1), (-1, 0)))
-    delta_r, delta_c = next(deltas)
-    for i in range(size**2):
-        matrix[r_index][c_index] = i + 1
+def spiral_matrix(matrix_size: int) -> list[list[int]]:
+    """
+    Create a spiral matrix of a given size.
+
+    Args:
+        matrix_size (int): The size of the matrix (number of rows and columns).
+
+    Returns:
+        list[list[int]]: A 2D list representing the spiral matrix.
+    """
+    spiral_matrix_ = [[None] * matrix_size for _ in range(matrix_size)]
+    row_index, col_index = 0, 0
+    direction_cycle = cycle(((0, 1), (1, 0), (0, -1), (-1, 0)))
+    delta_row, delta_col = next(direction_cycle)
+    for index in range(matrix_size**2):
+        spiral_matrix_[row_index][col_index] = index + 1
         if (
-            not 0 <= r_index + delta_r < size
-            or not 0 <= c_index + delta_c < size
-            or matrix[r_index + delta_r][c_index + delta_c] is not None
+            not 0 <= row_index + delta_row < matrix_size
+            or not 0 <= col_index + delta_col < matrix_size
+            or spiral_matrix_[row_index + delta_row][col_index + delta_col]
+            is not None
         ):
-            delta_r, delta_c = next(deltas)
-        r_index += delta_r
-        c_index += delta_c
-    return matrix
+            delta_row, delta_col = next(direction_cycle)
+        row_index += delta_row
+        col_index += delta_col
+    return spiral_matrix_

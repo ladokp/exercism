ae5174bbbee679b9316987c49be6eedfc5f55ace
Patrick Kodal <patrick@kodal.dev>
2024-09-15 21:44:31 +0200

    cüü: ellens-alien-game - iteration 2

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -4,21 +4,28 @@ namespace targets {
         Alien(int x, int y) {
           teleport(x, y);
         }
+    
         int get_health() {
           return health;
         }
+    
         bool hit() {
-            health--;
+            if (is_alive()) {
+                health--;
+            }
             return true;
         }
+    
         bool is_alive() {
             return health > 0;
         }
+    
         bool teleport(int x, int y) {
             x_coordinate = x;
             y_coordinate = y;
             return true;
         }
+    
         bool collision_detection(Alien opponent) {
             return x_coordinate == opponent.x_coordinate && y_coordinate == opponent.y_coordinate;
         }

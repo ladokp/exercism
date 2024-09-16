1bed0995541fbe1d9c3f0f2e26b16138b0f346d8
Patrick Kodal <patrick@kodal.dev>
2024-09-16 18:19:58 +0200

    python: book-store - iteration 2

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,7 +1,15 @@
-BUNDLE_PRICE = {1: 800, 2: 1520, 3: 2160, 4: 2560, 5: 3000}
+BUNDLE_PRICE: dict[int, int] = {1: 800, 2: 1520, 3: 2160, 4: 2560, 5: 3000}
 
 
-def price(book_quantities):
+def price(book_quantities: list[int]) -> int:
+    """Calculate the minimum price for the given quantities of books.
+
+    Args:
+        book_quantities (list[int]): A list of integers representing the quantities of books.
+
+    Returns:
+        int: The minimum price for the given quantities of books.
+    """
     book_quantities = sorted(
         quantity for quantity in book_quantities if quantity != 0
     )
@@ -18,5 +26,13 @@ def price(book_quantities):
     )
 
 
-def total(basket):
+def total(basket: list[int]) -> int:
+    """Calculate the total price for the given basket of books.
+
+    Args:
+        basket (list[int]): A list of integers representing the books in the basket.
+
+    Returns:
+        int: The total price for the given basket of books.
+    """
     return price(basket.count(n) for n in range(1, 6) if n in basket)

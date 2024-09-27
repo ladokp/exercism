357aa13de2b54b3f094a9627750f69714123d28f
Patrick Kodal <patrick@kodal.dev>
2024-09-27 19:56:18 +0200

    python: eliuds-eggs - iteration 7

--- python/eliuds-eggs/eliuds_eggs.py
+++ python/eliuds-eggs/eliuds_eggs.py
@@ -1,6 +1,18 @@
+"""
+Module for counting the number of 1s in the binary representation of an integer.
+"""
+
+
 def egg_count(display_value: int) -> int:
-    available_eggs = 0
-    while display_value > 0:
-        available_eggs += 1 if display_value % 2 else 0
-        display_value //= 2
-    return available_eggs
+    """
+    Counts the number of 1s in the binary representation of a given integer.
+
+    Args:
+        display_value (int): The integer to be evaluated.
+
+    Returns:
+        int: The count of 1s in the binary representation.
+    """
+    if display_value:
+        return (display_value & 1) + egg_count(display_value // 2)
+    return 0

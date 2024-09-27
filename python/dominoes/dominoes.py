d4ded2cd615a72a3d54b3d916d12ef726fd3e26f
Patrick Kodal <patrick@kodal.dev>
2024-09-27 19:35:10 +0200

    python: dominoes - iteration 2

--- python/dominoes/dominoes.py
+++ python/dominoes/dominoes.py
@@ -1,19 +1,40 @@
+"""
+This module contains a function to determine if a list of dominoes can form a chain.
+"""
+
+
 def can_chain(dominoes, chain=None):
+    """
+    Determine if the given list of dominoes can form a chain where each domino's
+    end matches the beginning of the next one.
+
+    Args:
+        dominoes (list): A list of tuples representing the dominoes.
+        chain (list, optional): The current chain being formed. Defaults to None.
+
+    Returns:
+        list: A chain of dominoes if possible, otherwise an empty list.
+    """
     if chain is None:
         chain = []
     if not dominoes:
         return chain
     last_number = chain[-1][-1] if chain else dominoes[0][-1]
-    for index, dominoes_ in enumerate(dominoes):
-        if last_number in dominoes_:
-            return_chain = can_chain(
+    for index, current_domino in enumerate(dominoes):
+        if last_number in current_domino:
+            attempted_chain = can_chain(
                 dominoes[:index] + dominoes[index + 1 :],
                 chain
                 + [
-                    dominoes_
-                    if dominoes_[0] == last_number
-                    else dominoes_[::-1]
+                    (
+                        current_domino
+                        if current_domino[0] == last_number
+                        else current_domino[::-1]
+                    )
                 ],
             )
-            if return_chain and return_chain[0][0] == return_chain[-1][-1]:
-                return return_chain
+            if (
+                attempted_chain
+                and attempted_chain[0][0] == attempted_chain[-1][-1]
+            ):
+                return attempted_chain

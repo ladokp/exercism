c27a4df7955762f894d347ee54463ef13f1096c9
Patrick Kodal <patrick@kodal.dev>
2024-10-12 21:37:24 +0200

    python: sum-of-multiples - iteration 3

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,3 +1,21 @@
+"""
+This module provides a function to calculate the sum of all numbers below a given limit
+that are multiples of any numbers in a specified list of multiples.
+"""
+
+
 def sum_of_multiples(limit, multiples):
-    """Return the energy points from level multiples the base values."""
-    return sum(i for i in range(1, limit) if any(j for j in multiples if j != 0 and i % j == 0))
+    """Calculate the sum of all numbers below a given limit that are multiples of any numbers in a specified list.
+
+    Args:
+        limit (int): The upper limit for the range of numbers to check.
+        multiples (list of int): A list of numbers to find multiples of.
+
+    Returns:
+        int: The sum of all numbers below the limit that are multiples of any numbers in the list.
+    """
+    return sum(
+        number
+        for number in range(1, limit)
+        if any(j for j in multiples if j != 0 and number % j == 0)
+    )

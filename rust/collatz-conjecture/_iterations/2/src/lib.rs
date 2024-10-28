ce06be94369d82063ebb4bde56001a533be2fdf2
Patrick Kodal <patrick@kodal.dev>
2024-10-28 19:34:07 +0100

    rust: collatz-conjecture

---
+++ rust/collatz-conjecture/_iterations/2/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn collatz(n: u64) -> Option<u64> {
+    if n == 1 {
+        return Some(0u64);
+    }
+    if n % 2 == 0 {
+        return Some(1u64 + collatz(n / 2)?)
+    }
+    Some(1u64 + collatz(n * 3 + 1)?)
+}
